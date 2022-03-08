1ec5801c58329f88199b17f4db84addfbda285e7
Federico Carboni <f.carboni98@gmail.com>
2022-03-08 12:01:33 +0100

    Telegram inspector
    But, It needs to start from a valid QR ("tg://login?token=[...]")

---
+++ fuzzqr/QRCodeFuzzer/inspectors/telegram.js
@@ -0,0 +1,31 @@
+class Inspector {
+    app_name = "telegram"
+    app_package = "org.telegram.messenger"
+    app_activity = "org.telegram.ui.LaunchActivity"
+
+    async goToScan(driver) {
+        let menu = await driver.findElement("xpath", '//android.widget.ImageView[@content-desc="Open navigation menu"]');
+        await driver.elementClick(menu.ELEMENT);
+
+        let settings = await driver.findElement("xpath", '//android.widget.FrameLayout[8]');
+        await driver.elementClick(settings.ELEMENT);
+
+        let devices = await driver.findElement("xpath", '//android.widget.FrameLayout[11]');
+        await driver.elementClick(devices.ELEMENT);
+
+        let qr = await driver.findElement("xpath", '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout[2]/androidx.recyclerview.widget.RecyclerView/android.widget.FrameLayout[1]/android.widget.TextView[2]');
+        await driver.elementClick(qr.ELEMENT);
+    }
+
+    async getResultView(driver) {
+        return await driver.findElement("xpath", '//android.widget.TextView[@text="OK"]');
+    }
+    
+    async goBackToScan(driver) {
+        let qr = await driver.findElement("xpath", '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout[2]/androidx.recyclerview.widget.RecyclerView/android.widget.FrameLayout[1]/android.widget.TextView[2]');
+        await driver.elementClick(qr.ELEMENT);
+    }
+
+}
+
+exports.Inspector = Inspector

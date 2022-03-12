cb1fc801cb17135356e0a08c73b760cb1394fb79
Mariano Sciacco <34033090+Maxelweb@users.noreply.github.com>
2022-03-12 18:34:25 +0100

    add boilerplate for sequential script and install script

---
+++ script/visualizer-sequential.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+## TODO: follow this https://github.com/Maxelweb/FuzzQRTestingUNIPD/issues/7
+
+# 1) get a list of all apps
+# 2) get argument with the working folder path
+# 3) execute sequentially all the apps invoking the command for the visualizer (remember 10 seconds cooldown for the last one and then exit!)

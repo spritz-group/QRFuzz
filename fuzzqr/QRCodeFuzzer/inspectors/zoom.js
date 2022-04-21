5b0f75658205aa97685a2844b8238bc777d1e9be
donadelden <denisdonadel@gmail.com>
2022-04-21 15:35:54 +0200

    Locate scan from ID

--- fuzzqr/QRCodeFuzzer/inspectors/zoom.js
+++ fuzzqr/QRCodeFuzzer/inspectors/zoom.js
@@ -7,7 +7,7 @@ class Inspector {
     	let settings = await driver.findElement("xpath", '//android.widget.RelativeLayout[@content-desc="More tab."]/android.widget.LinearLayout/android.widget.TextView');
         await driver.elementClick(settings.ELEMENT);
         
-        let scan = await driver.findElement("text", 'Scan QR Code');
+        let scan = await driver.findElement("id", '00000000-0000-2426-ffff-ffff00000198');
         await driver.elementClick(scan.ELEMENT);
         
        
@@ -21,7 +21,7 @@ class Inspector {
     	let cancel = await driver.findElement("xpath", '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.Button');
         await driver.elementClick(cancel.ELEMENT);
 
-        let scan = await driver.findElement("text", 'Scan QR Code');
+        let scan = await driver.findElement("id", '00000000-0000-2426-ffff-ffff00000198');
         await driver.elementClick(scan.ELEMENT);
         }
 

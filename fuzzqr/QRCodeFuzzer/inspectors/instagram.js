13cdec266d0373da9e42b65f48d31031b4c58df0
Mariano Sciacco <34033090+Maxelweb@users.noreply.github.com>
2022-07-10 22:46:50 +0200

    removed instagram inspectors not working

--- fuzzqr/QRCodeFuzzer/inspectors/instagram.js
+++ fuzzqr/QRCodeFuzzer/inspectors/instagram.js
@@ -3,15 +3,16 @@ class Inspector {
     app_package = "com.instagram.android";
     app_activity = "com.instagram.android.activity.MainTabActivity";
 
+    // 2022-07-10 - Instagram - commented part are not working
     async goToScan(driver) {
         let profile = await driver.findElement("id", "com.instagram.android:id/profile_tab");
         await driver.elementClick(profile.ELEMENT);
         
-        let opt = await driver.findElement("xpath", '//android.widget.Button[@content-desc="Options"]');
-        await driver.elementClick(opt.ELEMENT);
+        // let opt = await driver.findElement("xpath", '//android.widget.Button[@content-desc="Options"]');
+        // await driver.elementClick(opt.ELEMENT);
 
-        let qr = await driver.findElement("xpath", '//android.widget.Button[@content-desc="QR code"]');
-        await driver.elementClick(qr.ELEMENT);
+        // let qr = await driver.findElement("xpath", '//android.widget.Button[@content-desc="QR code"]');
+        // await driver.elementClick(qr.ELEMENT);
 
         let scan = await driver.findElement("id", "com.instagram.android:id/bottom_button");
         await driver.elementClick(scan.ELEMENT);

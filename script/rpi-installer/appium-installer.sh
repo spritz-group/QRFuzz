d0630dc0d8579874c9d9956963363f9814a5eea6
Mariano Sciacco <34033090+Maxelweb@users.noreply.github.com>
2022-07-10 21:56:17 +0200

    add android-sdk and export ANDROID_HOME

--- script/rpi-installer/appium-installer.sh
+++ script/rpi-installer/appium-installer.sh
@@ -3,7 +3,7 @@
 
 echo "[FUZZQR] Installing node, npm, openjdk, android-tools"
 sudo apt update -y
-sudo apt install -y nodejs npm openjdk-11-jre openjdk-11-jdk android-sdk-build-tools
+sudo apt install -y nodejs npm openjdk-11-jre openjdk-11-jdk android-sdk-build-tools android-sdk
 
 
 echo "[FUZZQR] Add npm global folder"
@@ -12,8 +12,10 @@ npm config set prefix '~/.npm-global'
 echo "export PATH=\"\$HOME/.npm-global/bin:\$PATH\"" >> ~/.bashrc
 source ~/.bashrc 
 
+echo "[FUZZQR] Add Android_Home"
+echo "export ANDROID_HOME=\"/usr/lib/android-sdk\"" >> ~/.bashrc
+source ~/.bashrc 
+
 echo "[FUZZQR] Installing appium globally and qr code fuzzer"
 npm install -g appium;
 (cd ../../fuzzqr/QRCodeFuzzer/; npm install)
-
-# TODO: add to path in .bashrc

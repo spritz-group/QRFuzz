5e6d8deca53259df380106c89adbb71b06dc1a50
Mariano Sciacco <34033090+Maxelweb@users.noreply.github.com>
2022-07-10 21:40:03 +0200

    add python3-tk and python-is-python3

--- script/rpi-installer/python-installer.sh
+++ script/rpi-installer/python-installer.sh
@@ -3,7 +3,7 @@
 
 echo "[FUZZQR] Installing python and pip"
 sudo apt update -y
-sudo apt install -y python3 python3-pip
+sudo apt install -y python3 python3-pip python3-tk python-is-python3
 
 echo "[FUZZQR] Installing QRCode generator requirements"
 pip install -r ../../fuzzqr/QRCodeGenerator/requirements.txt

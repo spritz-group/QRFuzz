dc5a3988702d812a065f7e490bbaf321af0b5a86
Mariano Sciacco <34033090+Maxelweb@users.noreply.github.com>
2022-07-19 19:42:52 +0200

    fix bancoposta missing element on backtoscan

--- fuzzqr/QRCodeFuzzer/inspectors/bancoposta.js
+++ fuzzqr/QRCodeFuzzer/inspectors/bancoposta.js
@@ -3,6 +3,8 @@ class Inspector {
     app_package = "posteitaliane.posteapp.appbpol";
     app_activity = "posteitaliane.posteapp.appbpol.ui.activity.SplashActivity";
 
+    // Note: only the first QR gives a different error (that is QR not recognized because does not start with the URL required).
+
     async goToScan(driver) {
     	// let scan = await driver.findElement("xpath", '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.widget.ScrollView/android.view.ViewGroup/androidx.recyclerview.widget.RecyclerView[1]/android.view.ViewGroup[1]');
 
@@ -18,11 +20,8 @@ class Inspector {
     	let ok = await driver.findElement("id", 'posteitaliane.posteapp.appbpol:id/md_buttonDefaultPositive');
         await driver.elementClick(ok.ELEMENT);
 
-    	// let scan = await driver.findElement("xpath", '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.widget.ScrollView/android.view.ViewGroup/androidx.recyclerview.widget.RecyclerView[1]/android.view.ViewGroup[1]');
-        // await driver.elementClick(scan.ELEMENT);  
-        
-        let scan = await driver.findElement("id", 'posteitaliane.posteapp.appbpol:id/access_qr');
-        await driver.elementClick(scan.ELEMENT);       
+        // let scan = await driver.findElement("id", 'posteitaliane.posteapp.appbpol:id/access_qr');
+        // await driver.elementClick(scan.ELEMENT);       
     }
 
 }

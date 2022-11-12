eb6f594aebdfd10a5cb86ffa3c1fe13d4abfd22c
Federico Carboni <f.carboni98@gmail.com>
2022-11-12 16:43:09 +0100

    chrome (ok, ma non refresha i qr)

--- fuzzqr/QRCodeFuzzer/inspectors/chrome.js
+++ fuzzqr/QRCodeFuzzer/inspectors/chrome.js
@@ -1,27 +1,30 @@
 class Inspector {
-    app_name = "bancoposta";
-    app_package = "posteitaliane.posteapp.appbpol";
-    app_activity = "posteitaliane.posteapp.appbpol.ui.activity.SplashActivity";
+    app_name = "chrome";
+    app_package = "com.android.chrome";
+    app_activity = "com.google.android.apps.chrome.Main";
 
     // Note: only the first QR gives a different error (that is QR not recognized because does not start with the URL required).
 
     async goToScan(driver) {
     	// let scan = await driver.findElement("xpath", '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.widget.ScrollView/android.view.ViewGroup/androidx.recyclerview.widget.RecyclerView[1]/android.view.ViewGroup[1]');
 
-        let scan = await driver.findElement("id", 'posteitaliane.posteapp.appbpol:id/access_qr');
+        let lens = await driver.findElement("id", 'com.android.chrome:id/lens_camera_button');
+        await driver.elementClick(lens.ELEMENT);   
+
+        let scan = await driver.findElement("id", 'com.google.android.googlequicksearchbox:id/lens_camera_cutout_image');
         await driver.elementClick(scan.ELEMENT);       
     }
 
     async getResultView(driver) {
-        return await driver.findElement("id", "posteitaliane.posteapp.appbpol:id/md_titleFrame");
+        return await driver.findElement("id", "com.android.chrome:id/lens_camera_button");
     }
     
-    async goBackToScan(driver) {
-    	let ok = await driver.findElement("id", 'posteitaliane.posteapp.appbpol:id/md_buttonDefaultPositive');
-        await driver.elementClick(ok.ELEMENT);
+    async goBackToScan(driver) {  
+        let lens = await driver.findElement("id", 'com.android.chrome:id/lens_camera_button');
+        await driver.elementClick(lens.ELEMENT);   
 
-        // let scan = await driver.findElement("id", 'posteitaliane.posteapp.appbpol:id/access_qr');
-        // await driver.elementClick(scan.ELEMENT);       
+        let scan = await driver.findElement("id", 'com.google.android.googlequicksearchbox:id/lens_camera_cutout_image');
+        await driver.elementClick(scan.ELEMENT);    
     }
 
 }

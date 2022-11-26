38c3f7f8be8a84486f9bc3bb03fe57b925a9f67e
Federico Carboni <f.carboni98@gmail.com>
2022-11-26 10:06:06 +0100

    changed chrome to google search

--- fuzzqr/QRCodeFuzzer/inspectors/chrome.js
+++ fuzzqr/QRCodeFuzzer/inspectors/chrome.js
@@ -1,14 +1,14 @@
 class Inspector {
     app_name = "chrome";
-    app_package = "com.android.chrome";
-    app_activity = "com.google.android.apps.chrome.Main";
+    app_package = "com.google.android.googlequicksearchbox";
+    app_activity = "com.google.android.googlequicksearchbox.SearchActivity";
 
     // Note: only the first QR gives a different error (that is QR not recognized because does not start with the URL required).
 
     async goToScan(driver) {
     	// let scan = await driver.findElement("xpath", '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.widget.ScrollView/android.view.ViewGroup/androidx.recyclerview.widget.RecyclerView[1]/android.view.ViewGroup[1]');
 
-        let lens = await driver.findElement("id", 'com.android.chrome:id/lens_camera_button');
+        let lens = await driver.findElement("id", 'com.google.android.googlequicksearchbox:id/googleapp_lens_icon');
         await driver.elementClick(lens.ELEMENT);   
 
         let scan = await driver.findElement("id", 'com.google.android.googlequicksearchbox:id/lens_camera_cutout_image');
@@ -16,15 +16,15 @@ class Inspector {
     }
 
     async getResultView(driver) {
-        return await driver.findElement("id", "com.android.chrome:id/lens_camera_button");
+        return await driver.findElement("id", "com.google.android.googlequicksearchbox:id/googleapp_lens_icon");
     }
     
     async goBackToScan(driver) {  
-        let lens = await driver.findElement("id", 'com.android.chrome:id/lens_camera_button');
+        let lens = await driver.findElement("id", 'com.google.android.googlequicksearchbox:id/googleapp_lens_icon');
         await driver.elementClick(lens.ELEMENT);   
 
         let scan = await driver.findElement("id", 'com.google.android.googlequicksearchbox:id/lens_camera_cutout_image');
-        await driver.elementClick(scan.ELEMENT);    
+        await driver.elementClick(scan.ELEMENT);     
     }
 
 }
